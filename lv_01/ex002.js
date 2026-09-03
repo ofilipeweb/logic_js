b0884cb4771b96bb30ee899b9d8e42ef88237390
Filipe Monteiro <filipemonteirodev@gmail.com>
2026-09-03 05:36:03 -0300

    Add exercise 002: Implement legal age check functionality

---
+++ lv_01/ex002.js
@@ -0,0 +1,10 @@
+//Legal age — Ask for the person's age and state whether they are of legal age (18+).
+ 
+let idade = 17;
+
+function legalAge(idade){
+    if(idade >= 18) return "Legal Age" 
+    else return "under age"
+};
+
+console.log(legalAge(idade));
